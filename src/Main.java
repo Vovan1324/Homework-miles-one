e6d78ed04124204b4687f0532e8aebc20133b85a
Vladimir <zaicev.94@mail.ru>
2022-11-14 20:56:54 +0300

    srs/Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 7000;
+
+        int miles = price / 20;
+        System.out.println("Бонус:" + miles);
+    }
+}
+
+
